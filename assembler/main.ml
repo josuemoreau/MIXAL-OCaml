06c66fa621ccbc94ba87bc8695a8d94921ddee01
Josué MOREAU <jsmr14.12@gmail.com>
2020-08-29 18:03:13 +0200

    suppression du type symbol dans main.ml

--- assembler/main.ml
+++ assembler/main.ml
@@ -1,28 +1,11 @@
 open Ast
 open Word
+open Format
 
 module StringMap = Map.Make(String)
 
-type symbol = Int of int | Future of int
-
-(* (\* On suppose la liste triée *\)
- * let rec find_local_symbol_before line = function
- *   | [] -> failwith "Symbole non défini"
- *   | loc :: [] -> if loc < line then loc else failwith "Symbole non défini"
- *   | locp :: loc :: symbols ->
- *     if locp < line && line <= loc then locp
- *     else find_local_symbol_before line (loc :: symbols)
- * 
- * (\* On suppose la liste triée *\)
- * let rec find_local_symbol_before line = function
- *   | [] -> failwith "Symbole non défini"
- *   | loc :: [] -> if loc < line then loc else failwith "Symbole non défini"
- *   | locp :: loc :: symbols ->
- *     if locp < line && line <= loc then locp
- *     else find_local_symbol_before line (loc :: symbols) *)
-
-let bind_symbol (sym: Ast.symbol) (value: symbol)
-    (bindings: symbol list StringMap.t) : symbol list StringMap.t =
+let bind_symbol (sym: Ast.symbol) (value: int)
+    (bindings: int list StringMap.t) : int list StringMap.t =
   match sym with
   | SymLocal i ->
     let s = string_of_int i ^ "h" in
@@ -32,7 +15,7 @@ let bind_symbol (sym: Ast.symbol) (value: symbol)
   | SymString s ->
     StringMap.add s [value] bindings
 
-let eval_symbol line bindings sym =
+let eval_symbol_loc line bindings sym =
   try
     match sym with
     | ESym s ->
@@ -42,8 +25,7 @@ let eval_symbol line bindings sym =
       let l = StringMap.find (string_of_int i ^ "h") bindings in
       List.hd l
     | ELocalSymF i ->
-      let l = StringMap.find (string_of_int i ^ "h") bindings in
-      Future (List.length l)
+      failwith ("Symbole " ^ string_of_int i ^ "f interdit dans l'adresse de cette instruction.")
     | _ -> assert (false)
   with Not_found ->
     failwith ("Symbol inconnu : " ^ match sym with
@@ -51,74 +33,69 @@ let eval_symbol line bindings sym =
       | ELocalSymB i | ELocalSymF i -> string_of_int i
       | _ -> assert (false))
 
-let int_of_symbol = function
-  | Int i -> i
-  | _ -> failwith "Le symbole est une référence future."
-
-let symbolop_of_op op a b =
-  match a, b with
-  | Int e1, Int e2 -> Int (op e1 e2)
-  | _ -> failwith "Il est interdit d'utiliser un symbole futur avec des opérateurs arithmétiques."
-
-let symadd   = symbolop_of_op ( + )
-let symsub   = symbolop_of_op ( - )
-let symmul   = symbolop_of_op ( * )
-let symdiv   = symbolop_of_op ( / )
-let symdivp  = symbolop_of_op ( fun a b -> a * word_size + b )
-let symfspec = symbolop_of_op ( fun a b -> a * 8 + b )
-
-let rec eval_expr line bindings = function
-  | EPos a -> eval_atomicexpr line bindings a
-  | ENeg a -> symsub (Int 0) (eval_atomicexpr line bindings a)
+let rec eval_expr eval_symbol line bindings = function
+  | EPos a -> eval_atomicexpr eval_symbol line bindings a
+  | ENeg a -> - (eval_atomicexpr eval_symbol line bindings a)
   | EBinary (BAdd, e, a)   ->
-    symadd (eval_expr line bindings e) (eval_atomicexpr line bindings a)
+    ( + ) (eval_expr eval_symbol line bindings e) (eval_atomicexpr eval_symbol line bindings a)
   | EBinary (BSub, e, a)   ->
-    symsub (eval_expr line bindings e) (eval_atomicexpr line bindings a)
+    ( - ) (eval_expr eval_symbol line bindings e) (eval_atomicexpr eval_symbol line bindings a)
   | EBinary (BMul, e, a)   ->
-    symmul (eval_expr line bindings e) (eval_atomicexpr line bindings a)
+    ( * ) (eval_expr eval_symbol line bindings e) (eval_atomicexpr eval_symbol line bindings a)
   | EBinary (BDiv, e, a)   ->
-    symdiv (eval_expr line bindings e) (eval_atomicexpr line bindings a)
+    ( / ) (eval_expr eval_symbol line bindings e) (eval_atomicexpr eval_symbol line bindings a)
   | EBinary (BDivP, e, a)  ->
-    symdivp (eval_expr line bindings e) (eval_atomicexpr line bindings a)
+    ( fun a b -> a * word_size + b ) (eval_expr eval_symbol line bindings e) (eval_atomicexpr eval_symbol line bindings a)
   | EBinary (BFSpec, e, a) ->
-    symfspec (eval_expr line bindings e) (eval_atomicexpr line bindings a)
-and eval_atomicexpr line bindings = function
-  | ENum i -> Int i
-  | EAsterisk -> Int line
+    ( fun a b -> a * 8 + b ) (eval_expr eval_symbol line bindings e) (eval_atomicexpr eval_symbol line bindings a)
+and eval_atomicexpr eval_symbol line bindings = function
+  | ENum i -> i
+  | EAsterisk -> line
   | sym -> eval_symbol line bindings sym
 
-let rec eval_wval word line bindings = function
-  | WAtomic a -> eval_atomicwval word line bindings a
+let rec eval_wval eval_symbol word line bindings = function
+  | WAtomic a -> eval_atomicwval eval_symbol word line bindings a
   | WMul (w, a) ->
-    eval_wval word line bindings w;
-    eval_atomicwval word line bindings a
-and eval_atomicwval word line bindings = function
+    eval_wval eval_symbol word line bindings w;
+    eval_atomicwval eval_symbol word line bindings a
+and eval_atomicwval eval_symbol word line bindings = function
   | WExpr (e, FEmpty) ->
-    set_word_part line word (int_of_symbol (eval_expr line bindings e)) 5
+    set_word_part line word (eval_expr eval_symbol line bindings e) 5
   | WExpr (e, FExpr f) ->
-    set_word_part line word (int_of_symbol (eval_expr line bindings e))
-      (int_of_symbol (eval_expr line bindings f))
+    set_word_part line word (eval_expr eval_symbol line bindings e)
+      (eval_expr eval_symbol line bindings f)
 
 let parse_loc =
   List.fold_left (fun (nbline, bindings) line ->
       match line with
       | SymDefInstr (sym, AssInstr i) when i.op = EQU ->
         let word = Word.empty () in
-        eval_wval word nbline bindings i.addr;
-        (nbline, bind_symbol sym (Int (Word.to_int word)) bindings)
+        eval_wval eval_symbol_loc word nbline bindings i.addr;
+        (nbline, bind_symbol sym (Word.to_int word) bindings)
       | SymDefInstr (sym, AssInstr i) when i.op = ORIG ->
         let word = Word.empty () in
-        eval_wval word nbline bindings i.addr;
+        eval_wval eval_symbol_loc word nbline bindings i.addr;
         let addr = Word.to_int word in
-        (addr + 1, bind_symbol sym (Int addr) bindings)
+        (addr, bind_symbol sym nbline bindings)
       | SymDefInstr (sym, _) ->
-        (nbline + 1, bind_symbol sym (Int nbline) bindings)
+        (nbline + 1, bind_symbol sym nbline bindings)
       | Instr (AssInstr i) when i.op = EQU -> (nbline, bindings)
       | Instr (AssInstr i) when i.op = ORIG ->
         let word = Word.empty () in
-        eval_wval word nbline bindings i.addr;
-        (Word.to_int word + 1, bindings)
+        eval_wval eval_symbol_loc word nbline bindings i.addr;
+        (Word.to_int word, bindings)
       | Instr _ -> (nbline + 1, bindings)) (0, StringMap.empty)
+
+let pp_intlist f l =
+  let rec aux f l =
+    match l with
+    | [] -> ()
+    | i :: [] -> fprintf f "%d" i
+    | i :: l -> fprintf f "%d; %a" i aux l in
+  fprintf f "[%a]\n" aux l
+
+let pp_bindings f b =
+  StringMap.iter (fun k l -> fprintf f "%s = %a" k pp_intlist l) b
 
 let () =
   if Array.length Sys.argv > 1 then begin
@@ -128,7 +105,7 @@ let () =
       let t = Parser.main Lexer.lexer lb in
       print_ast t;
       let nbline, bindings = parse_loc t in
-      StringMap.iter (fun k v -> Format.printf "%s = " k v) bindings
+      printf "%a@." pp_bindings bindings
     with _ ->
       let p = lb.lex_curr_p in
       Format.printf "Error : line %d, column %d" p.pos_lnum (p.pos_cnum - p.pos_bol)
